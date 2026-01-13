9e786e0707a176cf6c728e394406a37ca788bbe3
marloikhodi <deivid.serafim03@gmail.com>
2026-01-13 19:37:05 -0300

    feat: setup prisma ORM

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,13 @@
 import fastify from 'fastify'
+import { PrismaClient } from 'generated/prisma/client.js'
 
 export const app = fastify()
+
+const prisma = new PrismaClient()
+
+prisma.user.create({
+	data: {
+		name: 'deivid',
+		email: 'deivid.serafim@teste.com',
+	},
+})
--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,5 @@
 import { app } from './app.js'
-import { env } from './env'
+import { env } from './env/index.js'
 
 app
 	.listen({
