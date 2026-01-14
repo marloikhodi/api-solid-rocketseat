7e116bc76d7e6608192e93e29c8575b713e41fb1
marloikhodi <deivid.serafim03@gmail.com>
2026-01-14 12:22:40 -0300

    feat: error list

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -1,6 +1,7 @@
 import type { FastifyReply, FastifyRequest } from 'fastify'
 import { z } from 'zod'
 import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository.js'
+import { UserAlreadyExistsError } from '@/services/errors/user-already-exists-error.js'
 import { RegisterUseCase } from '@/services/register.js'
 
 export async function register(request: FastifyRequest, reply: FastifyReply) {
@@ -22,8 +23,11 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
 			password,
 		})
 	} catch (err) {
-		reply.status(409).send(err)
-	}
+		if (err instanceof UserAlreadyExistsError) {
+			reply.status(409).send({ message: err.message })
+		}
 
+		return reply.status(500).send() //fix
+	}
 	return reply.status(201).send()
 }
--- src/services/register.ts
+++ src/services/register.ts
@@ -1,5 +1,6 @@
 import { hash } from 'bcryptjs'
 import type { UsersRepository } from '@/repositories/users-repository.js'
+import { UserAlreadyExistsError } from './errors/user-already-exists-error.js'
 
 interface RegisterUseCaseRequest {
 	name: string
@@ -14,7 +15,7 @@ export class RegisterUseCase {
 		const userWithSameEmail = await this.usersRepository.findByEmail(email)
 
 		if (userWithSameEmail) {
-			throw new Error('E-mail already exists.')
+			throw new UserAlreadyExistsError()
 		}
 
 		const password_hash = await hash(password, 6)
