4d4717be6502218574bebf5ca809854379a470dc
marloikhodi <deivid.serafim03@gmail.com>
2026-02-06 16:40:05 -0300

    feat: auth process with JWT

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,14 @@
+import fastifyJwt from '@fastify/jwt'
 import fastify from 'fastify'
 import z, { ZodError } from 'zod'
 import { env } from './env/index.js'
 import { appRoutes } from './http/routes.js'
 
 export const app = fastify()
+
+app.register(fastifyJwt, {
+	secret: env.JWT_SECRET,
+})
 
 app.register(appRoutes)
 
--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,8 +1,12 @@
 import type { FastifyInstance } from 'fastify'
 import { authenticate } from './controllers/authenticate.js'
+import { profile } from './controllers/profile.js'
 import { register } from './controllers/register.js'
 
 export async function appRoutes(app: FastifyInstance) {
 	app.post('/users', register)
 	app.post('/sessions', authenticate)
+
+	// Authenticated
+	app.get('/me', profile)
 }
