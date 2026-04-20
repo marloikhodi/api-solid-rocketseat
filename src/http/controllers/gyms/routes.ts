fe484aadcbf7c24eba66ccd3b932b2796ae7bc13
marloikhodi <deivid.serafim03@gmail.com>
2026-04-19 22:04:00 -0300

    feat: RBAC configuration

--- src/http/controllers/gyms/routes.ts
+++ src/http/controllers/gyms/routes.ts
@@ -1,4 +1,5 @@
 import type { FastifyInstance } from 'fastify'
+import { verifyUserRole } from '@/http/middlewares/verify-user-role.js'
 import { verifyJWT } from '../../middlewares/verify-jwt.js'
 import { create } from './create.js'
 import { nearby } from './nearby.js'
@@ -10,5 +11,5 @@ export async function gymsRoutes(app: FastifyInstance) {
 	app.get('/gyms/search', search)
 	app.get('/gyms/nearby', nearby)
 
-	app.post('/gyms', create)
+	app.post('/gyms', { onRequest: [verifyUserRole('ADMIN')] }, create)
 }
